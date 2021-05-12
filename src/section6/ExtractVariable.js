fddd0d93b4e94ff505785dd9110999f76e8fd615
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-05-12 23:29:32 +0900

    extract basePrice variable

--- src/section6/ExtractVariable.js
+++ src/section6/ExtractVariable.js
@@ -2,7 +2,8 @@ function price(order) {
   /**
    * 価格 = 本体価格 - 数量割引 + 送料
    */
-  return order.quantity * order.itemPrice -
+  const basePrice = order.quantity * order.itemPrice
+  return basePrice -
     Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-    Math.min(order.quantity * order.itemPrice * 0.1, 100);
+    Math.min(basePrice * 0.1, 100);
 }
