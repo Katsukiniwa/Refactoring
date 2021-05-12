af2f84b7b5a7a97ac485c9cc4c4e6fef42f6ec75
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-05-12 23:36:12 +0900

    extract quantityDiscount variable

--- src/section6/ExtractVariable.js
+++ src/section6/ExtractVariable.js
@@ -2,8 +2,7 @@ function price(order) {
   /**
    * 価格 = 本体価格 - 数量割引 + 送料
    */
-  const basePrice = order.quantity * order.itemPrice
-  return basePrice -
-    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-    Math.min(basePrice * 0.1, 100);
+  const basePrice = order.quantity * order.itemPrice;
+  const quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+  return basePrice - quantityDiscount + Math.min(basePrice * 0.1, 100);
 }
