8dc826c6bad43707951919a79b52fe7586ac1b36
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-05-22 19:47:56 +0900

    define getter and setter function

--- src/section6/EncapsulateVariable.js
+++ src/section6/EncapsulateVariable.js
@@ -12,3 +12,14 @@ spaceship.owner = defaultOwner;
  * 更新するとき
  */
 defaultOwner = { firstName: 'NewSample', lastName: 'NewTest' };
+
+/*
+ * データを読み書きする関数を定義することで変数をカプセル化する
+ */
+function getDefaultOwner() {
+  return defaultOwner;
+}
+
+function setDefaultOwner(arg) {
+  defaultOwner = arg;
+}
