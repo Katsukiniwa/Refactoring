65b79d4269865b224b795ba2c794beb9c823e73e
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-06-13 14:21:33 +0900

    start encapslating record

---
+++ src/section7/1-Encapsulate-Record.js
@@ -0,0 +1,9 @@
+// P. 168~
+
+// 至る所で使われている
+const organization = { name: 'sample', country: "GB" };
+
+const result = `<h1>${organization.name}</>h1`;
+
+const newName = 'newSample'
+organization.name = newName;
