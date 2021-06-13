d9ae6c80ac3cac4fef3da414dd5d3841ab8b897c
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-06-13 14:39:06 +0900

    complete

--- src/section7/1-Encapsulate-Record.js
+++ src/section7/1-Encapsulate-Record.js
@@ -1,14 +1,42 @@
 // P. 168~
 
 // 至る所で使われている
-const organization = { name: 'sample', country: "GB" };
+const organization = new Organization({ name: 'sample', country: "GB" });
 
-const result = `<h1>${getRawDataOfOrganization.name}</>h1`;
+const result = `<h1>${getOrganization().name}</>h1`;
 
 const newName = 'newSample'
-getRawDataOfOrganization.name = newName;
+getOrganization().name = newName;
 
-// 変数のカプセル化
-function getRawDataOfOrganization() {
-  return organization;
+function getOrganization() {
+	return organization;
+}
+
+class Organization {
+	constructor(data) {
+		this._name = data.name;
+		this._country = data.country;
+	}
+
+	/**
+	 * @param {string} aString
+	 */
+	set name(aString) {
+		this._name = aString;
+	}
+
+	get name() {
+		return this._name;
+	}
+
+	/**
+	 * @param {string} aString
+	 */
+	set country(aString) {
+		this._country = aString;
+	}
+
+	get country() {
+		return this._country;
+	}
 }
